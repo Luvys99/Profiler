baaf8a413327f2e8b91c65365c7d467fd336dd00
영서 김 <mineboy0878@naver.com>
2026-06-05 22:19:58 +0900

    성능 및 가독성 개선 중

--- Profiler/Profiler.h
+++ Profiler/Profiler.h
@@ -1,86 +1,190 @@
 #pragma once
+
 #include <windows.h>
 #include <cstring>
 #include <iostream>
 
+// 매크로 "\"뒤에 공백들어가면 오류 바로 "\n"해야됨
+#define PROFILE \
+	static int _my_profile_id = -1; \
+	Profiler _p(__FUNCTION__, _my_profile_id)
+
 struct ProfileData
 {
-	long _flag = 0;
-	const char* _name = nullptr;
+	long _flag = 0; // 배열에 등록된 함수
+	const wchar_t* _name = nullptr;
 	double _totaltime = 0.0;
 	double _Min = 0;
 	double _Max = 0;
 	long _callcount = 0;
+
 };
 
-class ProfileManager
-{
-private:
-	ProfileData list[30];
-	int arridx = 0;
+// 전역 배열 선언
+ProfileData list[30];
 
-	// 구조체 내의 중요한 데이터기 때문에 함수 내부에서만 접근
-	void UpdataTimeData(int idx, double time);
-	void UpdateMinMax(int idx, double time);
-	// 외부에서 접근하는 함수들
+class Profiler
+{
 public:
+	Profiler()
+	{
+
+	}
+	~Profiler()
+	{
+
+	}
+
+	inline void Profiler_begin(const wchar_t* funcname) noexcept;
+	inline void Profiler_end(const wchar_t* funcname) noexcept;
+
 	// 이름을 받아서 인덱스를 반환하는 함수
-	int GetProfileID(const char* name);
+	inline int GetProfileID(const wchar_t* funcname) noexcept;
 	// 실행시간 측정한 데이터 저장
-	void SaveData(double time, int idx);
+	inline void SaveData(double time, int idx) noexcept;
 	// 파일 오픈 함수
-	FILE* OpenFile(const char* filename);
+	inline FILE* OpenFile(const wchar_t* filename) noexcept;
 	// 리포트 타이틀그리는 함수
-	void DrawProfileReportTitle(FILE* fp);
+	inline void DrawProfileReportTitle(FILE* fp) const noexcept;
 	// 리포트 그리는 함수
-	void DrawProfileReport(FILE* fp);
+	inline void DrawProfileReport(FILE* fp) const noexcept;
 	// 엔터누르면 한 번 호출하는 함수라 가독성 최우선
-	void WriteProfile(const char* filename);
+	inline void WriteProfile(const wchar_t* filename) noexcept;
 	// 초기화 함수 ( 올바른 측정을 위한 )
-	void ResetProfile();
-
-};
-
-// 전역 객체를 선언해서 여러 개의 Profile 객체들이 하나의 관리 객체에 데이터를 저장하게끔
-extern ProfileManager g_ProfileManager;
-
-class Profile
-{
+	inline void ResetProfile() noexcept;
 private:
-	//Freq 최초에 1회만 선언되게끔 static 선언
+	// 마이크로세컨드 또는 고성능포퍼먼스 타이머를 사용하기 위한 변수
 	static LARGE_INTEGER Freq;
 	LARGE_INTEGER Start;
 	LARGE_INTEGER End;
 	int _profileID;
 	double _time = 0;
-public:
-	//이름 저장하지 않고 함수 이름으로 바로 아이디 등록
-	Profile(const char* myname, int& staticID)
-	{
-		if (Freq.QuadPart == 0)
-		{
-			QueryPerformanceFrequency(&Freq);
-		}
 
-		if (staticID == -1)
-		{
-			staticID = g_ProfileManager.GetProfileID(myname);
-		}
-		_profileID = staticID;
-		QueryPerformanceCounter(&Start);
-	}
-	~Profile()
-	{
-		QueryPerformanceCounter(&End);
-		// 마이크로세컨즈 단위로 저장
-		_time = (double)((End.QuadPart - Start.QuadPart) * 1000000) / (double)Freq.QuadPart;
-		//실행 측정 함수 이름이랑, 시간 저장 함수
-		g_ProfileManager.SaveData(_time, _profileID);
-	}
+	int arridx = 0;
+
+	// 구조체 내의 중요한 데이터기 때문에 함수 내부에서만 접근
+	inline void UpdataTimeData(int idx, double time) noexcept;
+	inline void UpdateMinMax(int idx, double time) noexcept;
 
 };
 
-// 매크로 "\"뒤에 공백들어가면 오류 바로 "\n"해야됨
-#define PROFILE \
-	static int _my_profile_id = -1; \
-	Profile _p(__FUNCTION__, _my_profile_id)
+inline void Profiler_begin(const wchar_t* funcname) noexcept
+{
+
+
+}
+
+inline void Profiler_end(const wchar_t* funcname) noexcept
+{
+
+
+}
+
+
+inline void Profiler::UpdataTimeData(int idx, double time) noexcept
+{
+	list[idx]._totaltime += time;
+	list[idx]._callcount++;
+}
+
+inline void Profiler::UpdateMinMax(int idx, double time) noexcept
+{
+	if (time < list[idx]._Min)
+	{
+		list[idx]._Min = time;
+	}
+	else if (time > list[idx]._Max)
+	{
+		list[idx]._Max = time;
+	}
+}
+
+inline int Profiler::GetProfileID(const wchar_t* funcname) noexcept
+{
+	// 이미 등록된 이름이면 인덱스 반환
+	for (int idx = 0; idx < arridx; idx++)
+	{
+		//strcpy_s 같은 복사비용 늘어나는 함수 금지, if문으로 묶을 수 있으면 한번에 묶어서 cmp명령어 줄이기
+		if (list[idx]._flag == 1 && list[idx]._name == funcname)
+		{
+			return idx;
+		}
+	}
+
+	// 30개 이상 측정 안할꺼라서 배열 갯수 확인하는 비교문 제거
+	// 프로파일 배열 등록
+	list[arridx]._flag = 1;
+	list[arridx]._name = funcname;
+	list[arridx]._Max = 0;
+	list[arridx]._Min = 999999.0; //왜 999999.0 설정했지?
+	list[arridx]._totaltime = 0.0;
+	list[arridx]._callcount = 0;
+
+	// 등록했으면 갯수 증가
+	return arridx++;
+}
+
+inline void Profiler::SaveData(double time, int idx) noexcept
+{
+	if (idx < 0 || idx > 30) return;
+
+	// 시간 업데이트, Min, Max 업데이트 함수 분리
+	UpdataTimeData(idx, time);
+	UpdateMinMax(idx, time);
+}
+
+inline FILE* Profiler::OpenFile(const wchar_t* filename) noexcept
+{
+	FILE* fp = nullptr;
+	//fopen_s 반환값은 성공하면 0, 에러가 발생하면 에러코드(errno_t)
+	errno_t err = fopen_s(&fp, (char*)filename, "w");
+	if (err == 0)
+	{
+		std::cout << "File Open Success!!";
+		return fp;
+	}
+	else
+	{
+		std::cout << "File Open Failed!! Error Code : "<< err << "\n";
+		return nullptr;
+	}
+
+}
+
+inline void Profiler::DrawProfileReportTitle(FILE* fp) const noexcept
+{
+	fwprintf(fp, L"\t----------------------------------------------------------------------------------------------\n\n");
+	fwprintf(fp, L"\tName\t|\tAverage\t|\tMin\t|\tMax\t|\tCall\t|\n");
+	fwprintf(fp, L"\t----------------------------------------------------------------------------------------------\n");
+}
+
+inline void Profiler::DrawProfileReport(FILE* fp) const noexcept
+{
+	for (int idx = 0; idx < arridx; idx++)
+	{
+		fwprintf(fp, L"\t%s  |  %lfus\t|  %lfus\t|  %lfus\t|\t%d\n", list[idx]._name, list[idx]._totaltime / list[idx]._callcount,
+			list[idx]._Min, list[idx]._Max, list[idx]._callcount);
+	}
+	fwprintf(fp, L"\t---------------------------------------------------------------------------------------------\n");
+}
+
+inline void Profiler::WriteProfile(const wchar_t* filename) noexcept
+{
+	FILE* fp = OpenFile(filename);
+
+	DrawProfileReportTitle(fp);
+	DrawProfileReport(fp);
+	fclose(fp);
+}
+
+inline void Profiler::ResetProfile() noexcept
+{
+	for (int idx = 0; idx < arridx; idx++)
+	{
+		// 전체를 초기화 할 필요 없이 등록했던 것만 초기화
+		list[idx]._callcount = 0;
+		list[idx]._Max = 0.0;;
+		list[idx]._Min = 999999.0;
+		list[idx]._totaltime = 0.0;
+	}
+}
